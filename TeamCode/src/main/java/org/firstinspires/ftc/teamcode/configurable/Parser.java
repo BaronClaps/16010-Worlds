405b782bfc5052bd16f90dc04da3a5388011762a
BaronClaps <barbarhendo180@gmail.com>
2026-01-26 17:46:54 -0600

    feat: implement Scan class for template scanning and validation; refactor TemplateScanner logic

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/configurable/Parser.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/configurable/Parser.java
@@ -15,6 +15,7 @@ import dev.frozenmilk.sinister.sdk.apphooks.OnCreateEventLoop;
 import dev.frozenmilk.sinister.sdk.opmodes.SinisterRegisteredOpModes;
 import dev.frozenmilk.sinister.targeting.EmptySearch;
 import dev.frozenmilk.sinister.targeting.SearchTarget;
+import dev.frozenmilk.sinister.targeting.TeamCodeSearch;
 import dev.frozenmilk.util.graph.Graph;
 import dev.frozenmilk.util.graph.rule.AdjacencyRule;
 import dev.frozenmilk.util.graph.rule.AdjacencyRules;
@@ -105,94 +106,6 @@ public class Parser implements OnCreateEventLoop {
     public static class TemplateData {
         public final Map<String, Method> steps = new LinkedHashMap<>();
         public final List<Method> variants = new ArrayList<>();
-    }
-
-    private static final class TemplateScanner implements Scanner {
-        public static final TemplateScanner INSTANCE = new TemplateScanner();
-        private static final SearchTarget FILTER_SEARCH_TARGET = new EmptySearch().include("org.firstinspires.ftc.teamcode.configurable");
-
-        private TemplateScanner() {}
-
-        @NotNull
-        @Override
-        public SearchTarget getTargets() {
-            return FILTER_SEARCH_TARGET;
-        }
-
-        @Override
-        public void scan(@NotNull ClassLoader loader, @NotNull Class<?> cls) {
-            if (!cls.isAnnotationPresent(Auto.Template.class)) return;
-
-            TemplateData info = new TemplateData();
-
-            for (Method m : cls.getDeclaredMethods()) {
-                if (m.isAnnotationPresent(Auto.Step.class)) {
-                    boolean isStatic = Modifier.isStatic(m.getModifiers());
-
-                    if (!isStatic) {
-                        throw new IllegalArgumentException("Method " + cls.getCanonicalName() + "." + m.getName() +
-                                " annotated with @Auto.Step must be static");
-                    }
-
-                    if (m.getParameterCount() != 0) {
-                        throw new IllegalArgumentException("Method " + cls.getCanonicalName() + "." + m.getName() +
-                                " annotated with @Auto.Step must have 0 parameters");
-                    }
-
-                    if (!Command.class.isAssignableFrom(m.getReturnType())) {
-                        throw new IllegalArgumentException("Method " + cls.getCanonicalName() + "." + m.getName() +
-                                " annotated with @Auto.Step must return com.pedropathing.ivy.Command");
-                    }
-
-                    m.setAccessible(true);
-                    info.steps.put(m.getName(), m);
-                }
-
-                if (m.isAnnotationPresent(Auto.Variant.class)) {
-                    boolean isStatic = Modifier.isStatic(m.getModifiers());
-
-                    if (!isStatic) {
-                        throw new IllegalArgumentException("Method " + cls.getCanonicalName() + "." + m.getName() +
-                                " annotated with @Auto.Variant must be static");
-                    }
-
-                    if (m.getParameterCount() != 0) {
-                        throw new IllegalArgumentException("Method " + cls.getCanonicalName() + "." + m.getName() +
-                                " annotated with @Auto.Variant must have 0 parameters");
-                    }
-
-                    if (!OpMode.class.isAssignableFrom(m.getReturnType())) {
-                        throw new IllegalArgumentException("Method " + cls.getCanonicalName() + "." + m.getName() +
-                                " annotated with @Auto.Variant must return an OpMode instance");
-                    }
-
-                    m.setAccessible(true);
-                    info.variants.add(m);
-                }
-            }
-
-            if (Parser.templateData != null) {
-                System.out.println("Multiple templates found; keeping first discovered, ignoring: " + cls.getName());
-            } else {
-                Parser.templateData = info;
-                System.out.println("TemplateScanner discovered template: " + cls.getName() + " -> steps=" + info.steps.size() + " variants=" + info.variants.size());
-            }
-        }
-
-        @Override
-        public void unload(@NotNull ClassLoader loader, @NotNull Class<?> cls) {
-            // no-op
-        }
-
-        @Override
-        public @NotNull AdjacencyRule<Scanner, Graph<Scanner>> getLoadAdjacencyRule() {
-            return AdjacencyRules.independent();
-        }
-
-        @Override
-        public @NotNull AdjacencyRule<Scanner, Graph<Scanner>> getUnloadAdjacencyRule() {
-            return AdjacencyRules.independent();
-        }
     }
 
     @Override

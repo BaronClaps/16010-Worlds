47767842340972438ebaff7cdac3be688a518645
BaronClaps <126834072+BaronClaps@users.noreply.github.com>
2026-02-01 23:44:13 -0600

    Add Subsystems

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Intake.java
@@ -0,0 +1,83 @@
+package org.firstinspires.ftc.teamcode.subsystem;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.pedropathing.ivy.Command;
+import com.pedropathing.ivy.CommandBuilder;
+import com.pedropathing.ivy.commands.Commands;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+
+@Config
+public class Intake {
+    private final DcMotorEx i;
+    private final Servo k, b, t;
+    public static double off = 0;
+    public static double idle = 0.5;
+    public static double in = 1;
+    public static double out = -1;
+
+
+    public Intake(HardwareMap hardwareMap) {
+        i = hardwareMap.get(DcMotorEx.class, "i");
+        k = hardwareMap.get(Servo.class, "k");
+        b = hardwareMap.get(Servo.class, "b");
+        t = hardwareMap.get(Servo.class, "t");
+        set(0);
+    }
+
+    public void set(double power) {
+        i.setPower(power);
+    }
+
+    public void spinIn() {
+        set(in);
+    }
+
+    public void spinOut() {
+        set(out);
+    }
+
+    public void spinOff() {
+        set(off);
+    }
+
+    public void spinIdle() {
+        set(idle);
+    }
+
+    public CommandBuilder off() {
+        return Commands.instant(() -> set(off));
+    }
+
+    public CommandBuilder in() {
+        return Commands.instant(() -> set(in));
+    }
+
+    public CommandBuilder out() {
+        return Commands.instant(() -> set(out));
+    }
+
+    public CommandBuilder idle() {
+        return Commands.instant(this::spinIdle);
+    }
+
+    public CommandBuilder toggleIn() {
+        return Commands.instant(() -> {
+            if (i.getPower() != 0)
+                spinOff();
+            else
+                spinIn();
+        });
+    }
+
+    public CommandBuilder toggleOut() {
+        return Commands.instant(() -> {
+            if (i.getPower() != 0)
+                spinOff();
+            else
+                spinOut();
+        });
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Shooter.java
@@ -0,0 +1,97 @@
+package org.firstinspires.ftc.teamcode.subsystem;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.ivy.CommandBuilder;
+import com.pedropathing.ivy.commands.Commands;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import smile.interpolation.BilinearInterpolation;
+import smile.interpolation.Interpolation2D;
+
+@Config
+
+public class Shooter {
+    private DcMotorEx l, r;
+
+    private double t = 0;
+    public static double kS = 0.08, kV = 0.00039, kP = 0.01;
+
+    private boolean activated = true;
+
+    private static final double[] xs = {44, 72, 100};
+    private static final double[] ys = {10, 38, 66};
+    private static final double[][] closeVelocities = {
+            {1200, 1200, 1275},
+            {1275, 1275, 1350},
+            {1325, 1360, 1400}
+    };
+    public static final Interpolation2D closeInterpolation = new BilinearInterpolation(xs, ys, closeVelocities);
+    public Shooter(HardwareMap hardwareMap) {
+        l = hardwareMap.get(DcMotorEx.class, "sl");
+        r = hardwareMap.get(DcMotorEx.class, "sr");
+        l.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    public double getTarget() {
+        return t;
+    }
+
+    public double getVelocity() {
+        return r.getVelocity();
+    }
+
+    public void setPower(double p) {
+        l.setPower(-p);
+        r.setPower(-p);
+    }
+
+    public void off() {
+        activated = false;
+        setPower(0);
+    }
+
+    public void on() {
+        activated = true;
+    }
+
+    public void shooterToggle() {
+        activated = !activated;
+        if (!activated)
+            setPower(0);
+    }
+
+    public CommandBuilder toggle() {
+        return Commands.instant(this::shooterToggle);
+    }
+
+    public void setTarget(double velocity) {
+        t = velocity;
+    }
+
+    public void periodic() {
+        if (activated)
+            setPower((kV * getTarget()) + (kP * (getTarget() - getVelocity())) + kS);
+    }
+
+    public boolean atTarget() {
+        return Math.abs((getTarget()- getVelocity())) < 50;
+    }
+
+    public void forPose(Pose current, Pose target, boolean close) {
+        double xDistance = Math.abs(target.getX()-current.getX());
+        double yDistance = Math.abs(target.getY()-current.getY());
+
+        if (close)
+            setTarget(closeInterpolation.interpolate(xDistance, yDistance));
+        else
+            setTarget(1550);
+    }
+
+    public double timeInAir() {
+        return 1.0;
+    }
+
+}
+

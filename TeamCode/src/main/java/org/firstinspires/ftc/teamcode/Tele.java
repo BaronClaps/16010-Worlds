49db820d10fb41f60f4e1591424eb229d08fa0e7
BaronClaps <126834072+BaronClaps@users.noreply.github.com>
2026-04-04 00:30:37 -0500

    Tele :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele.java
@@ -1,1 +1,143 @@
-/// e
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.util.Timer;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import org.firstinspires.ftc.teamcode.util.Alliance;
+
+import static org.firstinspires.ftc.teamcode.Robot.defaultPose;
+
+public class Tele extends OpMode {
+    Alliance alliance;
+    Robot robot;
+
+    public boolean shoot = false, manual = false, field = false, shooting = false;
+    public double speed = 1, intakeOn = 0;
+    public static double shootTarget = 1250, timeToShoot = 0.5;
+    private final Timer shootTimer = new Timer();
+    MultipleTelemetry multipleTelemetry;
+
+    public Tele(Alliance alliance) {
+        this.alliance = alliance;
+    }
+
+    @Override
+    public void init() {
+        robot = new Robot(hardwareMap, alliance);
+        multipleTelemetry = new MultipleTelemetry(FtcDashboard.getInstance().getTelemetry(), telemetry);
+    }
+
+    public void start() {
+        robot.follower.setStartingPose(defaultPose);
+        robot.setShootTarget();
+        robot.follower.startTeleopDrive();
+        robot.intake.in();
+        robot.shooter.off();
+        robot.transfer.close();
+        robot.intake.lower();
+        shootTimer.resetTimer();
+    }
+
+    @Override
+    public void loop() {
+        robot.periodic();
+
+        if (field)
+            robot.follower.setTeleOpDrive(speed * -gamepad1.left_stick_y, speed * -gamepad1.left_stick_x, speed * -gamepad1.right_stick_x, false, robot.a == Alliance.BLUE ? Math.toRadians(180) : 0);
+        else
+            robot.follower.setTeleOpDrive(speed * -gamepad1.left_stick_y, speed * -gamepad1.left_stick_x, speed * -gamepad1.right_stick_x, true);
+
+        if (gamepad1.rightBumperWasPressed())
+            if (intakeOn == 1)
+                intakeOn = 0;
+            else
+                intakeOn = 1;
+
+        if (gamepad1.dpadDownWasPressed())
+            if (intakeOn == 2)
+                intakeOn = 0;
+            else
+                intakeOn = 2;
+
+        if (intakeOn == 1) {
+            robot.intake.in();
+            robot.transfer.in();
+        } else if (intakeOn == 2) {
+            robot.intake.out();
+            robot.transfer.out();
+        } else {
+            robot.intake.off();
+            robot.transfer.off();
+        }
+
+        if (shoot) {
+            robot.shooter.on();
+            if (manual) {
+                robot.shooter.setTarget(shootTarget);
+
+                if (gamepad2.dpadUpWasPressed())
+                    shootTarget += 50;
+                if (gamepad2.dpadDownWasPressed())
+                    shootTarget -= 50;
+
+                if (gamepad2.rightBumperWasPressed())
+                    robot.turret.manual(0.05);
+                if (gamepad2.leftBumperWasPressed())
+                    robot.turret.manual(-0.05);
+
+            } else {
+                double dist = robot.getShootTarget().distanceFrom(robot.follower.getPose());
+                boolean close = robot.follower.getPose().getY() > 48;
+                robot.shooter.forDistance(dist, close);
+                robot.turret.face(robot.getShootTarget(), robot.follower.getPose());
+            }
+        } else {
+            robot.shooter.off();
+        }
+
+        if (gamepad1.leftTriggerWasPressed())
+            shoot = !shoot;
+
+        if (gamepad1.rightTriggerWasPressed() && shoot) {
+            shooting = true;
+            shootTimer.resetTimer();
+            intakeOn = 1;
+            robot.transfer.open();
+        }
+
+        if (shooting && shootTimer.getElapsedTimeSeconds() > timeToShoot) {
+            shooting = false;
+            intakeOn = 1;
+            robot.transfer.close();
+        }
+
+        if (gamepad1.aWasPressed()) {
+            if (robot.a.equals(Alliance.BLUE)) {
+                robot.follower.setPose(new Pose(7.5, 8.5, Math.toRadians(0)).mirror());
+            } else {
+                robot.follower.setPose(new Pose(7.5, 8.5, Math.toRadians(0)));
+            }
+        }
+
+        if (gamepad2.bWasPressed())
+            manual = !manual;
+
+        if (gamepad1.yWasPressed())
+            field = !field;
+
+        multipleTelemetry.addData("Pose", robot.follower.getPose());
+        multipleTelemetry.addData("Goal Target", robot.getShootTarget());
+        multipleTelemetry.addLine();
+        multipleTelemetry.addData("Shooter Velocity", robot.shooter.getVelocity());
+        multipleTelemetry.addData("Shooter Target", robot.shooter.getTarget());
+        multipleTelemetry.addLine();
+        multipleTelemetry.addData("Manual", manual);
+        multipleTelemetry.addData("Shoot Target for Manual", shootTarget);
+        multipleTelemetry.addLine();
+        multipleTelemetry.addData("Field Centric?", field);
+
+
+    }
+}

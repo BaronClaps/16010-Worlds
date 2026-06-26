c67f65ee9be115e399eb817e88bf160da20c4409
BaronClaps <barbarhendo180@gmail.com>
2026-06-26 14:29:34 -0500

    weird ahh behavior

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/LocalizationTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/LocalizationTest.java
@@ -4,8 +4,11 @@ import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.math.Pose;
+import com.pedropathing.revhub.drivetrains.Mecanum;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import java.util.Arrays;
 
 @TeleOp
 public class LocalizationTest extends OpMode {
@@ -33,11 +36,14 @@ public class LocalizationTest extends OpMode {
         }
 
         follower.manual(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+        Mecanum m = (Mecanum) follower.drivetrain;
         follower.update();
         multipleTelemetry.addData("Loop Time Hz", 1000/loopTime);
         multipleTelemetry.addData("Mode", follower.mode());
         multipleTelemetry.addData("Idle?", follower.idle());
         multipleTelemetry.addData("Pose", follower.pose());
+        multipleTelemetry.addData("Manual Powers", follower.manualPowers);
+        multipleTelemetry.addData("Manual Output", Arrays.toString(Arrays.stream(m.wheelPowers).toArray()));
         multipleTelemetry.update();
     }
 }
